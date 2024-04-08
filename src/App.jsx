d88c30a62d3d6c52fc592b057774e2a84f316315
Axel <meister.axel@gmail.com>
2024-04-08 13:56:55 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Form from "./components/Form.jsx";
+import "./index.css";
+
+function App() {
+  const iframeListStyle = {
+    background: "transparent",
+    border: "1px solid #ccc",
+    width: "100%",
+    height: "700px",
+  };
+
+  const [showPopup, setShowPopup] = useState(false);
+
+  function togglePopup() {
+    setShowPopup(!showPopup);
+  }
+
+  return (
+    <main className="h-dvh container mx-auto px-5 flex flex-col justify-center items-center gap-3">
+      <iframe
+        src="https://airtable.com/embed/appVQeK2S27Ao22O2/shrxwBedOL86REAZz?backgroundColor=purple&layout=card&viewControls=on"
+        style={iframeListStyle}
+      ></iframe>
+      <button
+        className="bg-slate-200 rounded p-2 cursor-pointer"
+        onClick={togglePopup}
+        id="toggleButton"
+      >
+        Station hinzufügen
+      </button>
+      {showPopup && <Form toggleForm={togglePopup} />}
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "../index.css";
+
+function Form(props) {
+  const iframeFormStyle = {
+    background: "transparent",
+    border: "1px solid #ccc",
+    width: "100%",
+    height: "800px",
+  };
+
+  return (
+    <div
+      id="popup"
+      className="flex fixed inset-0 items-center justify-center z-50"
+    >
+      <div className="fixed inset-0 bg-gray-800 opacity-50"></div>
+      <button
+        onClick={props.toggleForm}
+        className="absolute top-2 left-2 bg-black rounded-full w-10 h-10 text-white flex items-center justify-center cursor-pointer"
+      >
+        X
+      </button>
+      <div className="container bg-white p-8 rounded shadow-sm z-10">
+        <iframe
+          src="https://airtable.com/embed/appVQeK2S27Ao22O2/pagzRDnO0wE9H3csR/form"
+          style={iframeFormStyle}
+        ></iframe>
+      </div>
+    </div>
+  );
+}
+
+export default Form;
