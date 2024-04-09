2f878571deeaf953ee1fac5242989ee7d2343b4b
Axel <meister.axel@gmail.com>
2024-04-09 11:19:54 +0200

    added Background Image

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Form from "./components/Form.jsx";
+import BgImage from "./assets/images/background-wave.svg";
 import "./index.css";
 
 function App() {
@@ -17,21 +18,24 @@ function App() {
   }
 
   return (
-    <main className="h-dvh container mx-auto px-5 flex flex-col justify-center items-center gap-3">
-      <h1 className="text-3xl font-pacifico">Übersicht</h1>
-      <iframe
-        src="https://airtable.com/embed/appVQeK2S27Ao22O2/shrxwBedOL86REAZz?backgroundColor=purple&layout=card&viewControls=on"
-        style={iframeListStyle}
-      ></iframe>
-      <button
-        className="bg-slate-200 rounded p-2 cursor-pointer"
-        onClick={togglePopup}
-        id="toggleButton"
-      >
-        Station hinzufügen
-      </button>
-      {showPopup && <Form toggleForm={togglePopup} />}
-    </main>
+    <>
+      <img src={BgImage} alt="" className="bg-image" />
+      <main className="h-dvh container mx-auto px-5 flex flex-col justify-center items-center gap-3">
+        <h1 className="text-3xl font-pacifico">Übersicht</h1>
+        <iframe
+          src="https://airtable.com/embed/appVQeK2S27Ao22O2/shrxwBedOL86REAZz?backgroundColor=purple&layout=card&viewControls=on"
+          style={iframeListStyle}
+        ></iframe>
+        <button
+          className="bg-slate-200 rounded p-2 cursor-pointer"
+          onClick={togglePopup}
+          id="toggleButton"
+        >
+          Station hinzufügen
+        </button>
+        {showPopup && <Form toggleForm={togglePopup} />}
+      </main>
+    </>
   );
 }
 
