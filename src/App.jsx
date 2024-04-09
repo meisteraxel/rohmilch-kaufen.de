64719a78632d5f8475a738b805d38e3af9361814
Axel <meister.axel@gmail.com>
2024-04-09 14:41:13 +0200

    adjusted background image

--- src/App.jsx
+++ src/App.jsx
@@ -20,36 +20,33 @@ function App() {
   }
 
   return (
-    <>
-      <img src={BgImage} alt="" className="bg-image" />
-      <main className="mx-auto px-5 flex flex-col items-center gap-3">
-        <h1 className="text-6xl font-inter-extrabold">
-          Rohmilch
-          <span className="font-pacifico block mt-2 relative left-24 text-5xl">
-            Übersicht
-          </span>
-        </h1>
-        <div className="container relative py-5 flex flex-col">
-          <img
-            src={Gras}
-            alt=""
-            className="w-24 h-24 absolute -z-10 -left-14 -top-1"
-          />
-          <button
-            className="self-end mb-4 bg-transparent cursor-pointer font-inter-bold text-sm underline"
-            onClick={togglePopup}
-            id="toggleButton"
-          >
-            Vorschlag einreichen
-          </button>
-          <iframe
-            src="https://airtable.com/embed/appVQeK2S27Ao22O2/shrxwBedOL86REAZz?backgroundColor=purple&layout=card&viewControls=on"
-            style={iframeListStyle}
-          ></iframe>
-        </div>
-        {showPopup && <Form toggleForm={togglePopup} />}
-      </main>
-    </>
+    <main className="mx-auto px-5 flex flex-col items-center gap-3">
+      <h1 className="text-6xl font-inter-extrabold mt-24">
+        Rohmilch
+        <span className="font-pacifico block mt-2 relative left-24 text-5xl">
+          Übersicht
+        </span>
+      </h1>
+      <div className="container relative py-5 flex flex-col">
+        <img
+          src={Gras}
+          alt=""
+          className="w-24 h-24 absolute -z-10 -left-14 -top-1"
+        />
+        <button
+          className="self-end mb-4 bg-transparent cursor-pointer font-inter-bold text-sm underline"
+          onClick={togglePopup}
+          id="toggleButton"
+        >
+          Vorschlag einreichen
+        </button>
+        <iframe
+          src="https://airtable.com/embed/appVQeK2S27Ao22O2/shrxwBedOL86REAZz?backgroundColor=purple&layout=card&viewControls=on"
+          style={iframeListStyle}
+        ></iframe>
+      </div>
+      {showPopup && <Form toggleForm={togglePopup} />}
+    </main>
   );
 }
 
