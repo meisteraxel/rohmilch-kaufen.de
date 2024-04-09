15c7cfb88ae9fcd15274f463efedea876222e898
Axel <meister.axel@gmail.com>
2024-04-09 11:57:41 +0200

    finalized first mvp

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Form from "./components/Form.jsx";
 import BgImage from "./assets/images/background-wave.svg";
+import Gras from "./assets/images/gras.png";
 import "./index.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     border: "1px solid #ccc",
     width: "100%",
     height: "700px",
+    "box-shadow": "0px 28px 40px 0px rgba(0,0,0,0.5)",
   };
 
   const [showPopup, setShowPopup] = useState(false);
@@ -20,22 +22,27 @@ function App() {
   return (
     <>
       <img src={BgImage} alt="" className="bg-image" />
-      <main className="h-dvh container mx-auto px-5 flex flex-col justify-center items-center gap-3">
-        <h1 className="text-6xl font-extrabold font-inter">
-          Rohmilch <br />
-          <span>Übersicht</span>
+      <main className="h-dvh mx-auto px-5 flex flex-col justify-center items-center gap-3">
+        <h1 className="text-6xl font-inter-extrabold">
+          Rohmilch
+          <span className="font-pacifico block mt-2 relative left-24 text-5xl">
+            Übersicht
+          </span>
         </h1>
-        <iframe
-          src="https://airtable.com/embed/appVQeK2S27Ao22O2/shrxwBedOL86REAZz?backgroundColor=purple&layout=card&viewControls=on"
-          style={iframeListStyle}
-        ></iframe>
-        <button
-          className="bg-slate-200 rounded p-2 cursor-pointer"
-          onClick={togglePopup}
-          id="toggleButton"
-        >
-          Station hinzufügen
-        </button>
+        <div className="container relative py-5 flex flex-col">
+          <img src={Gras} alt="" className="w-24 h-24 absolute" />
+          <button
+            className="self-end mb-4 bg-transparent cursor-pointer font-inter-bold text-sm underline"
+            onClick={togglePopup}
+            id="toggleButton"
+          >
+            Vorschlag einreichen
+          </button>
+          <iframe
+            src="https://airtable.com/embed/appVQeK2S27Ao22O2/shrxwBedOL86REAZz?backgroundColor=purple&layout=card&viewControls=on"
+            style={iframeListStyle}
+          ></iframe>
+        </div>
         {showPopup && <Form toggleForm={togglePopup} />}
       </main>
     </>
