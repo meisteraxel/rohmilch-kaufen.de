4c17b5c6de4a6069e6263ee91821fd508fd441ab
Axel <meister.axel@gmail.com>
2024-04-10 11:51:41 +0200

    moved Back Button into Popup Window

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,17 +8,17 @@ function Form(props) {
       className="flex fixed inset-0 items-center justify-center z-50"
     >
       <div className="fixed inset-0 bg-gray-800 opacity-50"></div>
-      <button
-        onClick={props.toggleForm}
-        className="absolute top-2 left-2 bg-black rounded-full w-10 h-10 text-white flex items-center justify-center cursor-pointer"
-      >
-        X
-      </button>
-      <div className="container bg-white p-8 rounded shadow-sm z-10">
+      <div className="container flex flex-col items-center bg-white p-8 rounded shadow-sm z-10">
         <iframe
           src="https://airtable.com/embed/appVQeK2S27Ao22O2/pagzRDnO0wE9H3csR/form"
-          className="bg-white w-full border h-[80vh]"
+          className="bg-white w-full border rounded h-[80vh]"
         ></iframe>
+        <button
+          onClick={props.toggleForm}
+          className="bg-[rgb(65,67,71)] rounded-md h-8 text-white font-semibold w-32 mt-6 hover:opacity-50 transition-opacity"
+        >
+          Back
+        </button>
       </div>
     </div>
   );
