d654bf993db1100967475683e6be9855acc1b8bc
Axel <meister.axel@gmail.com>
2024-04-11 00:01:34 +0200

    added footer component and adjusted some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Form from "./components/Form.jsx";
 import GrasIcon from "./assets/images/gras.png";
+import BubbleIcon from "./assets/images/gedanken.svg";
 import "./index.css";
 
 function App() {
@@ -11,19 +12,22 @@ function App() {
   }
 
   return (
-    <main className="mx-auto px-10 flex flex-col items-center gap-3">
-      <h1 className="text-6xl font-inter-extrabold mt-10">
+    <main className="container mx-auto px-10 flex flex-col items-center">
+      <h1 className="text-6xl text-center font-inter-extrabold">
         Rohmilch
         <br />
-        <span className="font-pacifico mt-2 relative left-24 text-5xl">
-          Übersicht
-        </span>
+        <span className="font-pacifico mt-2 text-5xl">Übersicht</span>
       </h1>
       <div className="container relative py-5 flex flex-col max-w-[1000px]">
         <img
           src={GrasIcon}
           alt=""
           className="w-24 h-24 absolute -z-10 -left-14 -top-1"
+        />
+        <img
+          src={BubbleIcon}
+          alt=""
+          className="w-24 h-24 absolute -z-10 -right-[90px] -top-[45px]"
         />
         <button
           className="self-end mb-4 bg-transparent cursor-pointer font-inter-bold text-sm underline hover:opacity-50 transition-opacity"
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Pageload from "./components/Pageload.jsx";
+import Footer from "./components/Footer.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Pageload />
     <App />
+    <Footer />
   </React.StrictMode>
 );
