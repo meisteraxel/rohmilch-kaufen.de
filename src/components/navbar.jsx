8c78997262dcaadb4de274ed3fd038f70b6c6f46
Axel <meister.axel@gmail.com>
2024-04-11 10:19:25 +0200

    styled navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import "../index.css";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="flex items-center justify-between font-inter-bold px-5">
+    <nav className="flex items-center justify-around font-inter-bold">
       <div>
-        <ul className="flex gap-4">
-          <li>
-            <p>Übersicht</p>
+        <ul className="flex">
+          <li className="px-8 py-8">
+            <Link to="/">Home</Link>
           </li>
-          <li>
-            <p>FAQ</p>
+          <li className="px-8 py-8">
+            <Link to="/faq">FAQ</Link>
           </li>
         </ul>
       </div>
-      <button>Station hinzufügen</button>
+      <button className="border-[3px] px-3 py-4 border-black">
+        Station hinzufügen
+      </button>
     </nav>
   );
 }
--- src/pages/faq.jsx
+++ src/pages/faq.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Footer from "../components/footer.jsx";
+import Navbar from "../components/navbar.jsx";
 import "../index.css";
 
 function Faq() {
-  return <Footer />;
+  return (
+    <>
+      <Navbar />
+      <Footer />;
+    </>
+  );
 }
 
 export default Faq;
