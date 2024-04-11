005ddaded060ee2b8a82a1540b64006c919bc4e7
Axel <meister.axel@gmail.com>
2024-04-11 11:02:39 +0200

    styled active links in navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import "../index.css";
 import CowLogo from "../assets/images/nav-cow-logo.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Navbar(props) {
+  const location = useLocation();
+
   return (
     <nav className="flex items-center justify-around font-inter-bold mt-3">
       <div>
-        <ul className="flex">
-          <li className="px-8 py-8">
+        <ul className="flex gap-20">
+          <li
+            className={
+              location.pathname === "/"
+                ? "border-b-4 border-black py-1"
+                : "py-1"
+            }
+          >
             <Link to="/">Home</Link>
           </li>
-          <li className="px-8 py-8">
+          <li
+            className={
+              location.pathname === "/faq"
+                ? "border-b-4 border-black py-1"
+                : "py-1"
+            }
+          >
             <Link to="/faq">FAQ</Link>
           </li>
         </ul>
