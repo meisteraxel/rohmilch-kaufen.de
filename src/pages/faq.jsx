71e80d4b9caad00ceb2672f568583158378efa64
Axel <meister.axel@gmail.com>
2024-04-11 11:56:50 +0200

    error with form component

--- src/pages/faq.jsx
+++ src/pages/faq.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
+import Footer from "../components/Footer.jsx";
+import Navbar from "../components/Navbar.jsx";
 import "../index.css";
 
 function Faq() {
