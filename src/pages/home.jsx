22a0d84b68f5fbe6585a65be87b50e19e0974d95
Axel <meister.axel@gmail.com>
2024-04-15 14:01:48 +0200

    added cookiebot workaround script

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -30,19 +30,6 @@ function Home() {
     setToggleView("Karte");
   }
 
-  useEffect(() => {
-    const cookieBotWrapper = document.getElementById("CookiebotDeclaration");
-    if (cookieBotWrapper) {
-      const script = document.createElement("script");
-      script.id = "CookieDeclaration";
-      script.type = "text/javascript";
-      script.async = true;
-      script.src = `https://consent.cookiebot.com/${process.env.NEXT_PUBLIC_COOKIEBOT_DOMAIN_GROUP_ID}/cd.js`;
-
-      cookieBotWrapper.appendChild(script);
-    }
-  }, []);
-
   return (
     <>
       <Navbar />
